9d273f91982ce4a2dcb079c885186558b568688e
Rowlet20 <101245778+Rowlet20@users.noreply.github.com>
2022-03-30 08:55:43 +0200

    Falling Ground
    Simplement aixó, he trigat dues putes hores.

--- Escenes/KinematicBody2D.gd
+++ Escenes/KinematicBody2D.gd
@@ -1,12 +1,14 @@
-extends Area2D
+extends KinematicBody2D
 
+var velocitat_base = 0
 var velocitat = Vector2.ZERO
-var gravetat = Vector2.DOWN * 980 #Si cada 100 pixels es un metre
+var gravetat = Vector2.DOWN * 500
 var falling = false
 
 func _physics_process(delta):
 	if falling == true:
-		position += Vector2(0,1)
+		velocitat += gravetat * delta
+		velocitat = move_and_slide(velocitat,Vector2.UP)
 	
 func _on_Area2D_body_entered(body):
 	falling = true
