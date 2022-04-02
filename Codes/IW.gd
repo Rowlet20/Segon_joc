ff7b3c4d194339db787ad6531e0ec37003cae953
Rowlet20 <101245778+Rowlet20@users.noreply.github.com>
2022-04-02 04:06:44 +0200

    Nivell 2 (fase 1)
    Un collo de coses

---
+++ Codes/IW.gd
@@ -0,0 +1,9 @@
+extends StaticBody2D
+
+
+func _ready():
+	pass 
+
+
+func _on_Nivell_2_body_entered(body):
+	position = Vector2(5420,1320)
--- Escenes/Personatge Fals.gd
+++ Escenes/Personatge Fals.gd
@@ -2,9 +2,7 @@ extends KinematicBody2D
 
 var velocitat_base = 300
 var velocitat = Vector2.ZERO
-var gravetat = Vector2.DOWN * 980 #Si cada 100 pixels es un metre
-var salt = Vector2.UP * 500
-var doble_salt = 1
+var gravetat = Vector2.DOWN * 700 #Si cada 100 pixels es un metre
 
 func _physics_process(delta):
 	velocitat.x = 0
@@ -12,18 +10,13 @@ func _physics_process(delta):
 		velocitat += Vector2.RIGHT * velocitat_base
 	if Input.is_action_pressed("left"):
 		velocitat += Vector2.LEFT * velocitat_base
-	if Input.is_action_just_pressed("up") and doble_salt != 0:
-		doble_salt -= 1
-		velocitat = salt
-		if doble_salt == 0:
-			modulate = Color(255,1,1)
-	if is_on_floor():
-		doble_salt = 1
-		modulate = Color(1,1,1)
 	if Input.is_action_pressed("return"):
 		position = Vector2(503,199)
 		
-	velocitat += gravetat * delta
+	if velocitat.y >= 800:
+		pass
+	else:
+		velocitat += gravetat * delta
 	velocitat = move_and_slide(velocitat, Vector2.UP)
 	
 	anima(velocitat)
